f15a1884272f6bf48e1c0ccb7e3d5e985195d836
nafdikzaydan <95949595+enadd@users.noreply.github.com>
2025-08-28 20:22:48 +0700

    Update sulidashboard.py

--- sulidashboard/sulidashboard.py
+++ sulidashboard/sulidashboard.py
@@ -16,7 +16,7 @@ sys.stderr.reconfigure(encoding='utf-8')
 
 st_autorefresh(interval=60 * 1000)
 st.set_page_config(layout="wide")
-st.title('Dashboard Distributor Suli 5 Tangerang Selatan', textposition='center')
+st.title('Dashboard Distributor Suli 5 Tangerang Selatan')
 # Refresh page every 60 seconds
 
 @st.cache_data(ttl=60)
@@ -133,6 +133,7 @@ def main():
     
 if __name__ == "__main__":
     main()
+
 
 
 
