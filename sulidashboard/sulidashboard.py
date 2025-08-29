ef98346ed47e863542d8d869824140a84d6deb70
nafdikzaydan <95949595+enadd@users.noreply.github.com>
2025-08-29 08:29:39 +0700

    Update sulidashboard.py

--- sulidashboard/sulidashboard.py
+++ sulidashboard/sulidashboard.py
@@ -67,6 +67,8 @@ def preprocessing(df_SL):
 df_sl = preprocessing(df_SL)
 
 def tagihan(df_sl):
+    df_sl['Omset'] = df_sl['Omset'].str.replace('Rp', '', regex=False)
+    df_sl['Omset'] = df_sl['Omset'].str.replace(',', '', regex=False)
     df_sl['Omset'] = pd.to_numeric(df_sl['Omset'], errors='coerce')
     df_sl = df_sl.dropna(subset=['Omset'])
     total_tagihan = df_sl.groupby('Nama Customer')['Omset'].sum().reset_index(name='Count')
@@ -143,5 +145,6 @@ def main():
     
 if __name__ == "__main__":
     main()
+
 
 
