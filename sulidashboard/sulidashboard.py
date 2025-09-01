21dd3455ccbd0b4ce4efcf552c3ee8422d6b5e75
nafdikzaydan <95949595+enadd@users.noreply.github.com>
2025-09-01 12:05:24 +0700

    Update sulidashboard.py

--- sulidashboard/sulidashboard.py
+++ sulidashboard/sulidashboard.py
@@ -93,10 +93,10 @@ def total_sales(df_sl):
 
 def jumlah_barang_per_bulan(df_sl):
     df_sl['Tanggal Order'] = pd.to_datetime(df_sl['Tanggal Order'], errors='coerce')
-    bulan = st.selectbox("Pilih bulan", list(range(1, 13)), format_func=lambda x: f"{x:02d}")
-    tahun = st.number_input("Pilih tahun", min_value=2020, max_value=2030, value=2025)
 
     filter_perbulan = df_sl[(df_sl['Tanggal Order'].dt.month == bulan) & (df_sl['Tanggal Order'].dt.year == tahun)]
+    bulan = st.selectbox("Pilih bulan", list(range(1, 13)), format_func=lambda x: f"{x:02d}")
+    tahun = st.number_input("Pilih tahun", min_value=2020, max_value=2030, value=2025)
 
     hasil = df_sl.groupby('Nama Barang')['Quantity'].sum().reset_index()
     return hasil
@@ -169,6 +169,7 @@ def main():
     
 if __name__ == "__main__":
     main()
+
 
 
 
