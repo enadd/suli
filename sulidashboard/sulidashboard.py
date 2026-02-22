6a777f342c84694366f46173adb83c481e36708a
nafdikzaydan <95949595+enadd@users.noreply.github.com>
2026-02-22 21:07:22 +0700

    Update sulidashboard.py

--- sulidashboard/sulidashboard.py
+++ sulidashboard/sulidashboard.py
@@ -70,7 +70,7 @@ total_grossprofit = calculate_total(df_revenue, 'Gross Profit')
 
 total_expense = calculate_total(df_expense, 'Jumlah')
 
-operating_margin = total_expense / total_revenue
+operating_margin = (total_revenue - total_expense) / total_revenue
 
 # Streamlit App
 def main():
@@ -94,6 +94,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
